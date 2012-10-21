fcd8211529deea7839937837eb9cf870bfdd2971
Ben Trevor <ben0500@gmail.com>
2012-10-21 10:48:51 -0500

    problem 179

--- ruby/pe179.rb
+++ ruby/pe179.rb
@@ -7,6 +7,10 @@ bigArray = Array.new(bigNumber + 1, 1)
 
 j = 2
 
+# Instead of using a numDivisors() function to calculate the number of divisors,
+# I'm looping through and adding one to every second number (for multiples of 2),
+# then adding one to every third number (for multiples of 3), etc.  This results
+# in an array such that bigArray[x] = numDivisors(x)
 for i in 2..bigNumber
   while j < bigNumber
     bigArray[j] += 1
@@ -17,6 +21,8 @@ end
 
 answer = 0
 
+# Then I just need to loop through the array to check how many times
+# bigArray[x] == bigArray[x + 1]
 for k in 1..bigNumber
   if bigArray[k] == bigArray[k+1]
     answer += 1
