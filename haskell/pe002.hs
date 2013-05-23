afee550e85ad93f0c0f4935178c357129c3df90f
Ben Trevor <benjamin.trevor@gmail.com>
2013-05-22 22:35:58 -0500

    Problem 2

--- haskell/pe002.hs
+++ haskell/pe002.hs
@@ -1,3 +1,8 @@
 module Pe002 where
 
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+evenFibs = [ xs | xs <- fibs, xs `mod` 2 == 0 ]
+
+evensUpTo limit = takeWhile (< limit) evenFibs
 
--- haskell/spec/pe002Spec.hs
+++ haskell/spec/pe002Spec.hs
@@ -7,4 +7,28 @@ import Pe002
 main :: IO ()
 
 main = hspec $ do
-  describe
+  describe "Fibonacci sequence" $ do
+    it "starts with two 1's" $ do
+      fibs!!1 `shouldBe` 1
+      fibs!!2 `shouldBe` 1
+
+    it "follows the first ten terms given" $ do
+      fibs!!3 `shouldBe` 2
+      fibs!!4 `shouldBe` 3
+      fibs!!5 `shouldBe` 5
+      fibs!!6 `shouldBe` 8
+      fibs!!7 `shouldBe` 13
+      fibs!!8 `shouldBe` 21
+      fibs!!9 `shouldBe` 34
+
+  describe "evenFibs" $ do
+    it "doesn't include odd numbers" $ do
+      evenFibs!!1 `shouldBe` 2
+      evenFibs!!2 `shouldBe` 8
+      evenFibs!!3 `shouldBe` 34
+
+  describe "evensUpTo" $ do
+    it "doesn't go past the limit" $ do
+      last (evensUpTo 10) `shouldBe` 8
+      last (evensUpTo 40) `shouldBe` 34
+
